cdc132bc55a5c69e568e8c4eaebdc358c46dea84
kevmach <kelvinmachinda6@gmail.com>
2025-10-23 15:16:44 +0200

    Fix vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is here
+  base: '/',
+  root: '.',
+  build: {
+    outDir: 'dist',
+  }
 })
